c3772cbefe1a2ebde87130548ff51112c939226e
Mark Montague <markmont@umich.edu>
2025-02-06 11:25:30 -0500

    load news items from news directory and/or main config file instead of hard-coding them in the script

--- script_context.py
+++ script_context.py
@@ -5,6 +5,7 @@ from rich.console import Console
 options = {}  # the parsed command line options
 config  = {}  # the parsed pantheon-sitehealth-emails.toml file
 plugin  = {}  # imported plugins (Python modules)
+news    = []  # list of news items to be displayed
 
 console = Console()
 
